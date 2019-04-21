00f2b8283f10969e327077499c71b3c0e8521539
Dreadpipe <benharris83@gmail.com>
2019-04-21 13:53:00 -0500

    Populated pathways, initialized brawlers.js, initialized server

---
+++ app/data/brawlers.js
@@ -0,0 +1,23 @@
+// DATA
+
+//This stores our brawler data.  For now, we use the dummy template.
+let brawlArray = [
+{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}];
+
+// Line for exporting the array in other files
+module.exports = brawlArray;
