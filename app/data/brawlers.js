325637f52654b30e3afa1a76631d8d39ab80f423
Dreadpipe <benharris83@gmail.com>
2019-04-23 19:13:38 -0500

    Compatibility logic exists, needs further testing- modal is responding appropriately, no result showing

--- app/data/brawlers.js
+++ app/data/brawlers.js
@@ -1,10 +1,44 @@
 // DATA
 
 //This stores our brawler data.  For now, we use the dummy template.
-let brawlArray = [
+let brawlers = [
 {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Conan",
+    "photo": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/arnie-920x584.png",
+    "scores": [
+        3,
+        1,
+        1,
+        4,
+        5,
+        5,
+        4,
+        3,
+        5,
+        5
+    ]
+},
+
+{
+    "name": "Hulk Hogan",
+    "photo": "https://www.gannett-cdn.com/-mm-/587d0546cd49b2f3174d0ad4ea1917baf6110e07/c=0-39-650-528/local/-/media/Phoenix/2014/05/19/0a4bae56caf3367904c2344c50ed2ed62ba884ee.jpg?width=534&height=401&fit=crop",
+    "scores": [
+        5,
+        5,
+        2,
+        3,
+        2,
+        3,
+        2,
+        5,
+        4,
+        3
+    ]
+},
+
+{
+    "name": "Giant Chicken",
+    "photo": "https://i.ytimg.com/vi/hNX_oPdP8Ec/hqdefault.jpg",
     "scores": [
         5,
         1,
@@ -17,7 +51,9 @@ let brawlArray = [
         4,
         1
     ]
-}];
+}
+
+];
 
 // Line for exporting the array in other files
-module.exports = brawlArray;
+module.exports = brawlers;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,7 +20,54 @@ module.exports = function (app) {
 
     // This posts new brawler data to our array
     // eventually will handle compatibility logic
+    // Compatibility assist courtesy of Mich @ https://github.com/mchlltt/
     app.post("/api/brawlers", function (req, res) {
+
+        var thisUser = req.body;
+        var differences = [];
+
+        // If there is more than one friend to compare to
+        if (brawlData.length > 1) {
+            // Step through these potential fighters
+            brawlData.forEach(function(user) {
+                var totalDifference = 0;
+
+                // For each score, compare the scores and add the absolute value of the difference to the total difference.
+                for (var i = 0; i < thisUser.scores.length; i++) {
+                    var otherScore = user.scores[i];
+                    var thisScore = thisUser.scores[i];
+                    var difference = +otherScore - +thisScore;
+                    totalDifference += Math.abs(difference);
+                }
+
+                differences.push(totalDifference);
+            });
+
+            // Find the minimum difference score
+            var minimumDifference = Math.min.apply(null, differences);
+
+            // Since there may be more than one potential fighter with that score, create an array
+            var bestMatches = [];
+
+            // For each item in differences, if it is equal to the minimumDifference, add the corresponding brawlData to the bestMatches array
+            for (var i = 0; i < differences.length; i++) {
+                if (differences[i] === minimumDifference) {
+                    bestMatches.push(brawlData[i]);
+                }
+            }
+
+            // Then send bestMatches to the client.
+            res.json(bestMatches);
+        // If there is only one friend to compare to, skip all that work and just send back that user
+        } else {
+            res.json(brawlData);
+        }
+
+        // Once you're done comparing, add the new user to the potential brawler data.
+        brawlData.push(thisUser);
+
+
+
 
     });
 };
